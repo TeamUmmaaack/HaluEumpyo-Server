5a7fa02f6bea5f2a268f316ed41c47fbc5bbecaa
bsorinnn <bxxsxrxx@gmail.com>
2022-05-19 11:56:43 +0900

    [FEAT] calendar db getCalendar 월별 검색 로직 추가 (#8)

--- functions/api/routes/calendar/CalendarGet.js
+++ functions/api/routes/calendar/CalendarGet.js
@@ -5,22 +5,26 @@ const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { calendarDB } = require('../../../db');
-const { default: axios } = require('axios');
-const { response } = require('express');
-
-
+const dayjs = require('dayjs');
 
 module.exports = async (req, res) => {
   const { userId } = req.params;
-  //유저 아이디가 없는 경우임
-if (!userId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  const { date } = req.query;
+
+  //유저 아이디와 오늘 날짜 없는 경우임
+  if (!userId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  if (!date) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
 
   let client;
+
+  // 오늘 날짜를 기준으로 월 시작과 마지막 검색
+  const startDate = dayjs(date).startOf('month').format('YYYY-MM-DD');
+  const endDate = dayjs(date).endOf('month').format('YYYY-MM-DD');
 
   try {
     client = await db.connect(req);
 
-    const calendar = await calendarDB.getCalendar(client, userId);  //userId를 통해 캘린더 디비에서 가져옴
+    const calendar = await calendarDB.getCalendar(client, userId, startDate, endDate); //userId를 통해 캘린더 디비에서 가져옴
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CALENDER_SUCCESS, calendar));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
@@ -31,4 +35,3 @@ if (!userId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode
     client.release();
   }
 };
-
--- functions/db/calendar.js
+++ functions/db/calendar.js
@@ -5,16 +5,18 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
  * @캘린더 불러오기
  */
 
+const getCalendar = async (client, userId, startDate, endDate) => {
+  const { rows } = await client.query(
+    `
+      SELECT *
+      FROM diary
+      WHERE user_id = $1 
+      AND created_at BETWEEN $2 AND $3
+      ORDER BY created_at
+    `,
+    [userId, startDate, endDate],
+  );
+  return convertSnakeToCamel.keysToCamel(rows); //전체 리스트를 가져오고싶어서 rows
+};
 
-  const getCalendar = async (client, userId) => {
-    const { rows } = await client.query(
-      `
-      SELECT * FROM "diary" 
-      WHERE user_id = $1
-      `,
-      [userId], 
-    );
-    return convertSnakeToCamel.keysToCamel(rows);  //전체 리스트를 가져오고싶어서 rows
-  };
-
-  module.exports = { getCalendar};
+module.exports = { getCalendar };
