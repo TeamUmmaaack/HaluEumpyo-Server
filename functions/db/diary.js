e6f0427cb253c18427487ccf9490c4e7c3a2040c
bsorinnn <bxxsxrxx@gmail.com>
2022-05-28 04:19:30 +0900

    [FIX] 일기 추가 db 로직 수정 (#13)

--- functions/db/diary.js
+++ functions/db/diary.js
@@ -5,13 +5,13 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
  * @일기 작성
  */
 
-const postDiary = async (client, userId, content, emotionId) => {
+const postDiary = async (client, userId, content, emotionId, musicId) => {
   const { rows } = await client.query(
     `INSERT INTO diary (content, user_id, emotion_id, music_id)
         VALUES ($1, $2, $3, $4)
         RETURNING content, user_id, emotion_id, music_id
         `,
-    [content, userId, emotionId, 1],
+    [content, userId, emotionId, musicId],
   );
 
   return convertSnakeToCamel.keysToCamel(rows[0]);
