e798324457c5a42ea082806fda9d1aa0d11aa70c
bsorinnn <bxxsxrxx@gmail.com>
2022-05-31 03:12:51 +0900

    [ADD] 토큰 관련 response message 추가 (#13)

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -2,6 +2,13 @@ module.exports = {
   NULL_VALUE: '필요한 값이 없습니다',
   OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
 
+  INTERNAL_SERVER_ERROR: '서버 내부 오류입니다.',
+
+  // 토큰
+  TOKEN_EMPTY: '토큰이 없습니다.',
+  TOKEN_EXPIRED: '토큰이 만료되었습니다.',
+  TOKEN_INVALID: '유효하지 않은 토큰입니다.',
+
   // 회원가입
   CREATED_USER: '회원 가입 성공',
   DELETE_USER: '회원 탈퇴 성공',
@@ -11,7 +18,7 @@ module.exports = {
   // 로그인
   LOGIN_SUCCESS: '로그인 성공',
   LOGIN_FAIL: '로그인 실패',
-  INVAILD_EMAIL: '이메일 형식이 맞지 않습니다.',
+  INVALID_EMAIL: '이메일 형식이 맞지 않습니다.',
   NO_USER: '존재하지 않는 회원입니다.',
   MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
 
