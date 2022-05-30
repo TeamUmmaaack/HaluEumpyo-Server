742addbf1d179f8e50ec063425b6b7cf5a5a115c
bsorinnn <bxxsxrxx@gmail.com>
2022-05-30 22:01:47 +0900

    [ADD] 로그인 회원가입 관련 responseMessage 추가 (#15)

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -6,10 +6,12 @@ module.exports = {
   CREATED_USER: '회원 가입 성공',
   DELETE_USER: '회원 탈퇴 성공',
   ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+  INVALID_PASSWORD: '비밀번호 형식이 잘못되었습니다. 패스워드는 최소 6자리의 문자열이어야 합니다.',
 
   // 로그인
   LOGIN_SUCCESS: '로그인 성공',
   LOGIN_FAIL: '로그인 실패',
+  INVAILD_EMAIL: '이메일 형식이 맞지 않습니다.',
   NO_USER: '존재하지 않는 회원입니다.',
   MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
 
@@ -17,7 +19,7 @@ module.exports = {
   ADD_DIARY_SUCCESS: '일기 작성 성공',
 
   // 캘린더 불러오기
-    READ_CALENDER_SUCCESS: '캘린더 불러오기 성공',
+  READ_CALENDER_SUCCESS: '캘린더 불러오기 성공',
 
   // 프로필 조회
   READ_PROFILE_SUCCESS: '프로필 조회 성공',
