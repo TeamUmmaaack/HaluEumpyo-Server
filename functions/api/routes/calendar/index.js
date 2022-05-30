c1504f2d4bae8292e55e376b93aebd88bec8320b
bsorinnn <bxxsxrxx@gmail.com>
2022-05-31 03:15:32 +0900

    [FIX] 캘린더 router jwt 검증 로직 추가 및 router 명 변경 (#17)

--- functions/api/routes/calendar/index.js
+++ functions/api/routes/calendar/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { checkUser } = require('../../../middlewares/auth');
 
-router.get('/:userId', require('./CalendarGET')); // 일정 기한 내 Calendar 가져오기
-router.get('/:userId/diary', require('./CalendarContentGET')); // 유저가 작성한 모든 Calendar 가져오기
+router.get('/', checkUser, require('./CalendarContentGET')); // 유저가 작성한 모든 Calendar 가져오기
+router.get('/detail', checkUser, require('./CalendarGET')); // 일정 기한 내 Calendar 가져오기
 
 module.exports = router;
