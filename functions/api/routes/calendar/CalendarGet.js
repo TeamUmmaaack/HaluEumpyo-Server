7530d4c09dfd7db93364ad3889f1892dcefd8226
bsorinnn <bxxsxrxx@gmail.com>
2022-06-01 04:17:01 +0900

    [FIX] 월별 캘린더 조회 api 오류 수정 (#17)
    - utc에 맞춰 저장된 db 시간 kst로 변경하는 로직 추가

--- functions/api/routes/calendar/CalendarGet.js
+++ functions/api/routes/calendar/CalendarGet.js
@@ -6,10 +6,12 @@ const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { calendarDB } = require('../../../db');
 const dayjs = require('dayjs');
+var utc = require('dayjs/plugin/utc');
+var timezone = require('dayjs/plugin/timezone');
 
 /**
  *  @route GET /calendar/detail
- *  @desc 캘린더 날짜별 조회
+ *  @desc 월별 캘린더 조회
  *  @access Private
  */
 
@@ -23,20 +25,28 @@ module.exports = async (req, res) => {
 
   let client;
 
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+
   // 오늘 날짜를 기준으로 월 시작과 마지막 검색
-  const startDate = dayjs(date).startOf('month').format('YYYY-MM-DD');
-  const endDate = dayjs(date).endOf('month').format('YYYY-MM-DD');
+  const startDate = dayjs(date).startOf('month').format('YYYY-MM-DD HH:mm:ss');
+  const endDate = dayjs(date).endOf('month').format('YYYY-MM-DD HH:mm:ss');
+
+  // 서버 timestamp는 utc 기준이므로 kst에서 9시간 뺴주기
+  const kstStartDate = dayjs(startDate).subtract(9, 'hour').format('YYYY-MM-DD HH:mm:ss');
+  const kstEndDate = dayjs(endDate).subtract(9, 'hour').format('YYYY-MM-DD HH:mm:ss');
 
   try {
     client = await db.connect(req);
 
-    const calendar = await calendarDB.getCalendar(client, userId, startDate, endDate); //userId, startDate, endDate를 통해 기간 내에 속한 캘린더 디비에서 가져옴
+    const calendar = await calendarDB.getCalendar(client, userId, kstStartDate, kstEndDate); //userId, startDate, endDate를 통해 기간 내에 속한 캘린더 디비에서 가져옴
     calendar.map((obj) => {
       /**
        * createdAt format 수정
        */
-      obj.createdAt = dayjs(`${obj.createdAt}`).format('YYYY-MM-DD ddd HH:mm');
+      obj.createdAt = dayjs(`${obj.createdAt}`).tz('Asia/Seoul').format('YYYY-MM-DD ddd HH:mm');
     });
+    console.log(calendar);
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CALENDER_SUCCESS, calendar));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
--- functions/db/calendar.js
+++ functions/db/calendar.js
@@ -7,20 +7,20 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 const getCalendar = async (client, userId, startDate, endDate) => {
   const { rows } = await client.query(
-    `SELECT DISTINCT d.id, 
-    d.content,
-    d.emotion_id,
-    m.title,
-    m.singer,
-    m.cover,
-    m.url,
-    d.created_at
-    FROM diary AS d
-    LEFT OUTER JOIN music as m ON d.music_id = m.id 
-    WHERE d.user_id = $1
-    AND d.created_at BETWEEN $2 AND $3
-    ORDER BY d.created_at
-  `,
+    `SELECT DISTINCT d.id,
+      d.content,
+      d.emotion_id,
+      m.title,
+      m.singer,
+      m.cover,
+      m.url,
+      d.created_at
+      FROM diary AS d
+      LEFT OUTER JOIN music as m ON d.music_id = m.id
+      WHERE d.user_id = $1
+      AND d.created_at BETWEEN $2 AND $3
+      ORDER BY d.created_at
+    `,
     [userId, startDate, endDate],
   );
   return convertSnakeToCamel.keysToCamel(rows); //전체 리스트를 가져오고싶어서 rows
