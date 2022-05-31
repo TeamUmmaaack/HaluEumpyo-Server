c127677987189ac87389b6aecd01d8c3e15d0b46
bsorinnn <bxxsxrxx@gmail.com>
2022-06-01 04:11:47 +0900

    [FEAT] 유저 db 토큰 저장 및 업데이트 로직 구현 (#17)

--- functions/db/user.js
+++ functions/db/user.js
@@ -1,3 +1,4 @@
+const { refreshToken } = require('firebase-admin/app');
 const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
@@ -8,6 +9,18 @@ const getUserById = async (client, userId) => {
       WHERE id = $1
       `,
     [userId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+const getUserByRefreshToken = async (client, refreshToken) => {
+  const { rows } = await client.query(
+    `
+    SELECT * from "user" u
+    WHERE refresh_token = $1
+    AND is_deleted = FALSE
+    `,
+    [refreshToken],
   );
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
@@ -24,18 +37,31 @@ const getUserByFirebaseId = async (client, firebaseId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-const addUser = async (client, email, name, firebaseId) => {
+const addUser = async (client, email, name, firebaseId, refreshToken) => {
   const { rows } = await client.query(
     `
     INSERT INTO "user" 
-    (email, username, id_firebase)
+    (email, username, id_firebase, refresh_token)
     VALUES
-    ($1, $2, $3)
+    ($1, $2, $3, $4)
     RETURNING *
     `,
-    [email, name, firebaseId],
+    [email, name, firebaseId, refreshToken],
   );
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-module.exports = { getUserById, getUserByFirebaseId, addUser };
+const updateRefreshToken = async (client, userId, newRefreshToken) => {
+  const { rows } = await client.query(
+    `
+      UPDATE "user"
+      SET refresh_token = $2 
+      WHERE id = $1
+      AND is_deleted = FALSE
+      RETURNING *
+      `,
+    [userId, newRefreshToken],
+  );
+};
+
+module.exports = { getUserById, getUserByFirebaseId, getUserByRefreshToken, addUser, updateRefreshToken };
