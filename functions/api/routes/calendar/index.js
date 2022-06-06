aed727eb3e33c2174d10efc87c6888793e660a77
bsorinnn <bxxsxrxx@gmail.com>
2022-06-06 22:01:59 +0900

    [FIX] router 명 변경 (#21)

--- functions/api/routes/calendar/index.js
+++ functions/api/routes/calendar/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
-router.get('/', checkUser, require('./calendarContentGET')); // 유저가 작성한 모든 Calendar 가져오기
-router.get('/detail', checkUser, require('./calendarGET')); // 일정 기한 내 Calendar 가져오기
+router.get('/', checkUser, require('./CalendarContentGET')); // 유저가 작성한 모든 Calendar 가져오기
+router.get('/detail', checkUser, require('./CalendarGet')); // 일정 기한 내 Calendar 가져오기
 
 module.exports = router;
