bbfe34fd5263a09c66e9a56a9fb02e3c374440f1
bsorinnn <bxxsxrxx@gmail.com>
2022-10-02 01:23:58 +0900

    [REFACTOR] 음악 추천 수정(#23)
    - 추천 음악과 유사한 음악으로 분리
    - 일기 중복 설정 방지

--- functions/api/routes/diary/DiaryPOST.js
+++ functions/api/routes/diary/DiaryPOST.js
@@ -4,8 +4,16 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-const { diaryDB, musicDB } = require('../../../db');
+const { diaryDB, musicDB, calendarDB } = require('../../../db');
 const axios = require('axios');
+const dayjs = require('dayjs');
+require('dayjs/locale/ko');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.locale('ko');
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 /**
  *  @route POST /diary
@@ -17,7 +25,11 @@ module.exports = async (req, res) => {
   const { userId } = req.user;
   const { content } = req.body;
   let emotionId,
-    musicId = -1;
+    recommendedMusicId = -1;
+  let similarMusicsIds = [];
+
+  const startDate = dayjs().tz('Asia/Seoul').startOf('day').subtract(9, 'hour').format('YYYY-MM-DD HH:mm:ss');
+  const endDate = dayjs().tz('Asia/Seoul').endOf('day').subtract(9, 'hour').format('YYYY-MM-DD HH:mm:ss');
 
   if (!content) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
 
@@ -30,7 +42,8 @@ module.exports = async (req, res) => {
         .then((response) => {
           console.log(response.data);
           emotionId = response.data.emotion;
-          musicId = response.data.musicId;
+          recommendedMusicId = response.data.recommended_music;
+          similarMusicsIds = response.data.similar_musics;
           return;
         })
         .catch((e) => {
@@ -44,13 +57,37 @@ module.exports = async (req, res) => {
   };
 
   try {
-    await flaskResult(content);
     client = await db.connect(req);
+    // 중복 일기 검사
+    let flag = true;
+    const userDiary = await calendarDB.getCalendar(client, userId, startDate, endDate);
+    if (userDiary?.length) {
+      console.log(userDiary);
+      // 이미 작성된 일기가 있다면
+      flag = false;
+    }
 
-    const diaryData = await diaryDB.postDiary(client, userId, content, emotionId, musicId);
-    const musicData = await musicDB.getMusicById(client, musicId);
+    if (flag) {
+      // 유저가 해당 날짜에 일기를 작성하지 않을 경우
+      await flaskResult(content);
+      const similarMusicData = [];
 
-    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ADD_DIARY_SUCCESS, musicData));
+      await diaryDB.postDiary(client, userId, content, emotionId, recommendedMusicId);
+      const recommendedMusicData = await musicDB.getMusicById(client, recommendedMusicId);
+      for (const similarMusicId of similarMusicsIds) {
+        const similarMusic = await musicDB.getMusicById(client, similarMusicId);
+        similarMusicData.push(similarMusic[0]);
+      }
+
+      const responseData = {
+        recommendedMusic: recommendedMusicData,
+        similarMusics: similarMusicData,
+      };
+
+      return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ADD_DIARY_SUCCESS, responseData));
+    } else {
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.DUPLICATE_DIARY));
+    }
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
